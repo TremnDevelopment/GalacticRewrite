0ff890a98b0d4b030a7dd52df4d61f5f0e4b070b
KeyToFailure <juliagirlhehe@gmail.com>
2024-07-16 13:22:50 -1000

    Update gameVariables.lua
    Added teamcheck, healthcheck for aimbot yay

--- gameVariables.lua
+++ gameVariables.lua
@@ -1,6 +1,8 @@
 return {
     ['Arsenal'] = {
         ['AimbotEnabled'] = false,
+        ['AimbotTeamCheck'] = true,
+        ['AimbotHealthCheck'] = true,
         ['ESPEnabled'] = false,
         ['ESPNametagEnabled'] = false,
         ['isAiming'] = false
